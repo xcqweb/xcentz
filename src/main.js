c791aa63dd9230ace8d0b9af690b98453078df96
xcqweb <384928755@qq.com>
2019-04-30 12:00:20 +0800

    ok

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import '@/assets/styles/reset.less'
+import '@/assets/styles/main.less'
 
+
+import 'iview/dist/styles/iview.css';
+import '@/assets/js/iview'
 Vue.config.productionTip = false
 
+
+Vue.mixin({
+  data(){
+    return{
+      userInfo:Object.freeze(JSON.parse(localStorage.getItem('userInfo'))),
+    }
+  },
+  methods:{
+    isNull(obj){
+        return Object.prototype.toString.call(obj) === '[object Null]'
+    }
+  }
+})
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
