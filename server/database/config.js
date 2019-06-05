2a0e636e77794787bb703d12b1420d396276c06d
xcqweb <384928755@qq.com>
2019-06-05 16:11:43 +0800

    edit

--- server/database/config.js
+++ server/database/config.js
@@ -1,13 +1,10 @@
 module.exports = {
-    // HOST:'47.88.50.243',
-    // USERNAME:'develep',
-    // PASSWORD:'xcentz_1',
-    // DATABASE:'xcentz',
+    host:'47.88.50.243',
+    user:'develep',
+    password:'xcentz_1',
+    database:'xcentz',
 
-    HOST:'localhost',
-    USERNAME:'root',
-    PASSWORD:'xcq123456',
-    DATABASE:'blog',
+    
 
     secretOrPrivateKey:'ahs3ysdsdf34ef65eery6ghagwe76w'
 }
--- server/database/index.js
+++ server/database/index.js
@@ -1,20 +1,52 @@
 const mysql = require('mysql')
 const config = require('./config')
 
-const pool = mysql.createPool({
-	host: config.HOST,
-	user: config.USERNAME,
-	password: config.PASSWORD,
-    database: config.DATABASE,
-    multipleStatements:true,
-    dateStrings:true,
-    connectTimeout:30000,
-    connectionLimit:30
-})
+var poolCluster = mysql.createPoolCluster();
+
+let masterConfig = {
+    host:'localhost',
+    user:'root',
+    password:'xcq123456',
+    database:'blog',
+}
+
+poolCluster.add(config);
+poolCluster.add('MASTER', masterConfig); 
+
+// const pool = mysql.createPool({
+// 	host: config.HOST,
+// 	user: config.USERNAME,
+// 	password: config.PASSWORD,
+//     database: config.DATABASE,
+//     multipleStatements:true,
+//     dateStrings:true,
+//     connectTimeout:30000,
+//     connectionLimit:30
+// })
 
 let query = function(sql, values) {
 	return new Promise((resolve, reject) => {
-        pool.getConnection(function(err, connection) {
+        poolCluster.getConnection('MASTER',function(err, connection) {
+            if (err) {
+                reject(err)
+            } else {
+                connection.query(sql, values, (error, rows) => {
+                    connection.release() //release将连接返回池中  供他们再次调用
+                    if (error) {
+                        reject(error)	
+                    } else {
+                        resolve(rows)
+                    }
+                })
+            }
+        })
+	})
+}
+
+
+let query_blog = function(sql, values) {
+	return new Promise((resolve, reject) => {
+        poolCluster.getConnection(function(err, connection) {
             if (err) {
                 reject(err)
             } else {
@@ -32,5 +64,6 @@ let query = function(sql, values) {
 }
 
 module.exports = {
-	query
+    query,
+    query_blog
 }
